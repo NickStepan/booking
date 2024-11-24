3a2339c41a056ebf5f0d25f65e18359d703ebf15
NickStepan <NickStepan>
2024-11-24 18:55:16 +0200

    Project created

---
+++ booking/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Room(models.Model):
+    number = models.CharField(max_length=30)
+    capacity = models.IntegerField()
+    location = models.TextField()
+
+    def __str__(self):
+        return f"{self.number}-{self.capacity}"
+    
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name="bookings")
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}-{self.room}"
+    
+    
